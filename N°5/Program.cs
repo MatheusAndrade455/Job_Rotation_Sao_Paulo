0ebace0e1ba2759d9b70302a878b9fdb57ee4a6d
Matheus Andrade <63677287+MatheusAndrade455@users.noreply.github.com>
2022-06-21 19:40:14 -0300

    Add files via upload

---
+++ N°5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra;
+            int j = 1;                                
+
+            Console.WriteLine("Escreva uma palavra ou frase para ser invertida:");
+            palavra = Console.ReadLine();
+
+            char[] characters = palavra.ToCharArray();            
+            char[] inverso = palavra.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++)            
+            {               
+               inverso[i] = characters[characters.Length - j];   
+               j++;           
+            }
+            Console.Write("\nA string invertida fica: ");
+            Console.Write(inverso);            
+        }
+    }
+}
